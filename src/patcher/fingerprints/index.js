824693e25ab91c3043a417b824a2eeb734dc15c7
reis <reis@turkeylist.gq>
2022-10-11 06:54:10 +0000

    Dynamically find class dex number

--- src/patcher/fingerprints/index.js
+++ src/patcher/fingerprints/index.js
@@ -1,23 +1,45 @@
-import { readFileSync, existsSync } from 'node:fs';
+import { readFileSync, existsSync, readdirSync } from 'node:fs';
 import { join } from 'node:path';
 import { toObject } from '../../smaliParser/parser.js';
 
-export default function getFingerPrint(fingerprint) {
-	const path = join(
-		process.cwd(),
-		`revancedjs-cache/${
-			fingerprint.classDexNumber === 0
-				? 'smali'
-				: `smali_classes${fingerprint.classDexNumber}`
-		}/${fingerprint.path}`
+function exists(dir, fingerprint) {
+	return existsSync(
+		join(process.cwd(), `revancedjs-cache/${dir}/${fingerprint.path}`)
 	);
+}
 
-	if (!existsSync(path)) {
+export default function getFingerPrint(fingerprint) {
+	const fingerprintResult = {
+		found: false,
+		index: 'smali'
+	};
+	for (const folderName of readdirSync(
+		join(process.cwd(), 'revancedjs-cache')
+	)) {
+		if (folderName.startsWith('smali')) {
+			if (folderName === 'smali') {
+				if (exists(folderName, fingerprint)) {
+					fingerprintResult.found = true;
+				}
+			} else {
+				if (exists(folderName, fingerprint)) {
+					fingerprintResult.found = true;
+					fingerprintResult.index = folderName;
+				}
+			}
+		}
+	}
+
+	if (!fingerprintResult.found) {
 		console.log(
 			'Could not find the class file! Did you input the wrong class number?'
 		);
 		return null;
 	}
+	const path = join(
+		process.cwd(),
+		`revancedjs-cache/${fingerprintResult.index}/${fingerprint.path}`
+	);
 	const file = readFileSync(path, 'utf8');
 	const smaliObject = toObject(file);
 
